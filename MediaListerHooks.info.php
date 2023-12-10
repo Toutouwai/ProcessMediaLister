d6fcd11c0812d043cb84daa0abc46e39419481f0
Robin Sallis <Toutouwai@users.noreply.github.com>
2023-12-10 16:45:08 +1300

    Separate module info files

---
+++ MediaListerHooks.info.php
@@ -0,0 +1,12 @@
+<?php namespace ProcessWire;
+
+$info = array(
+	'title' => 'Media Lister: Hooks',
+	'summary' => 'Hooks for Media Lister.',
+	'version' => '0.1.0',
+	'author' => 'Robin Sallis',
+	'href' => 'https://github.com/Toutouwai/ProcessMediaLister',
+	'icon' => 'th-large',
+	'autoload' => 'template=admin',
+	'requires' => 'ProcessWire>=3.0.0, PHP>=7.0.0, ProcessMediaLister',
+);
---
+++ ProcessMediaLister.info.php
@@ -0,0 +1,22 @@
+<?php namespace ProcessWire;
+
+$info = array(
+	'title' => 'Media Lister',
+	'summary' => 'Lists images and files in the site.',
+	'version' => '0.1.0',
+	'author' => 'Robin Sallis',
+	'href' => 'https://github.com/Toutouwai/ProcessMediaLister',
+	'icon' => 'th-large',
+	'requires' => 'ProcessWire>=3.0.226, PHP>=7.0.0',
+	'installs' => 'MediaListerHooks',
+	'page' => array(
+		'name' => 'media-lister',
+		'title' => 'Media Lister',
+		'parent' => 'setup',
+	),
+	'permission' => 'media-lister',
+	'permissions' => array(
+		'media-lister' => 'Use the Media Lister module'
+	),
+	'useNavJSON' => true,
+);
