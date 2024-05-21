23cfdc175cbf56f648c4aa4126cd80b2148f3553
Robin Sallis <Toutouwai@users.noreply.github.com>
2024-05-21 12:52:41 +1200

    v0.1.3
    Use Field::getTable() to get table name. Closes #3

--- ProcessMediaLister.info.php
+++ ProcessMediaLister.info.php
@@ -3,7 +3,7 @@
 $info = array(
 	'title' => 'Media Lister',
 	'summary' => 'Lists images and files from across the site in a sortable and filterable table.',
-	'version' => '0.1.2',
+	'version' => '0.1.3',
 	'author' => 'Robin Sallis',
 	'href' => 'https://github.com/Toutouwai/ProcessMediaLister',
 	'icon' => 'th-large',
